5cc830bd28d14936ae5b8ffad1c9db66168cfc2b
Ajmalajjuca <ajmalajju313786@gmail.com>
2024-09-13 10:00:46 +0530

    fixd some error

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ require('dotenv').config();
     passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8080/auth/google/callback"
+      callbackURL: "https://avocado.ajuu.online/auth/google/callback"
     },
     async function(accessToken, refreshToken, profile, done) {
       try {
